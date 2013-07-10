402726b900b9ed084e6632996b3f0cd71b99caa2
octmb <octmb.94@gmail.com>
2013-07-10 01:23:58 -0500

    4éme preuve

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html').toString());
+  response.send(fs.readFile('index.html','utf8'));
 });
 
 var port = process.env.PORT || 5000;
