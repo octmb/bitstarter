f2bddc4c90c9972b73368579b5782880cb4dc4b2
octmb <octmb.94@gmail.com>
2013-07-10 01:19:36 -0500

    Deuxiéme preuve en essayant de lire depuis index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html', 'r'));
+  response.send(buf.toString(fs.readFile('index.html')));
 });
 
 var port = process.env.PORT || 5000;
